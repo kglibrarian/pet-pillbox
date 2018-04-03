85eab09c44c0f9aae5537d0cbce4c66cd5aed97c
Karen E Gutzman <keg827@fsm.northwestern.edu>
2018-04-02 21:19:36 -0500

    added javascript

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
             console.log(searchTerm);
            
             // Construct a queryURL using the search term
-                var queryURL = "https://dailymed.nlm.nih.gov/dailymed/services/v2/drugnames.json?drug_name=" + searchTerm ;
+                var queryURL = "https://dailymed.nlm.nih.gov/dailymed/services/v2/spls.json?drug_name=" + searchTerm + "&pagesize=5&page=2" ;
     
             // Performing an AJAX request with the queryURL
             $.ajax({
@@ -29,7 +29,7 @@ $(document).ready(function(){
                 console.log(response);
                 
                 // storing the data from the AJAX request in the results variable
-                console.log(response.data.drug_name);
+                console.log(response.data.title);
                 var results = response.data;
     
                 // Looping through each result item
@@ -39,7 +39,7 @@ $(document).ready(function(){
                 var drugDiv = $("<div>");
       
                 // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text(results[i].drug_name);
+                var p = $("<p>").text(results[i].title);
                 
                 //Creating an add button next to each drug name
                 var b = $("<button class= 'add'>").text("+");
@@ -48,6 +48,7 @@ $(document).ready(function(){
     
                // Appending the paragraph to the drugDiv
                 drugDiv.append(p);
+                // drugDiv.prepend(b);
                 
               // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
               $(".drugs-appear-here").prepend(drugDiv);
@@ -65,25 +66,28 @@ $(document).ready(function(){
              
             
              //Create a variable called searchTerm to hold the value that the user entered into the search box
-             var addedDrugTerm = $('.add').parent().get(0).innerText;
-        
-             
+             var addedDrugTerm = $(this).parent().get(0).innerText.slice(1);
+
+            
              //Console log the addedDrugTerm  
-             console.log(addedDrugTerm);
+             console.log("This is the added drug term: " + addedDrugTerm);
     
              // Creating and storing a div tag
              var usersDrugDiv = $("<div>");
       
-             // Creating a paragraph tag with the item the user wants to add to their list
+            //  Creating a paragraph tag with the item the user wants to add to their list
              var p = $("<p>").text(addedDrugTerm);
+             console.log(p);
              
-             //Creating a delete button next to each drug name
-             var b = $("<button class='delete'>").text("-").attr("delete");
-             p.prepend(b);
+            //  Creating a delete button next to each drug name
+             var bb = $("<button class='delete'>").text("-");
+             
+             p.prepend(bb);
     
     
             // Appending the paragraph to the drugDiv
-             usersDrugDiv.append(p);
+            // usersDrugDiv.prepend(bb);
+            usersDrugDiv.append(p);
              
            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
            $(".user-drugs-appear-here").prepend(usersDrugDiv);
