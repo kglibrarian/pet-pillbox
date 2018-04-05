e5cee4a077849511c0bcf8a7be5df39a0902d54b
Sana Afreen <s.afreen42789@GMAIL.COM>
2018-04-04 21:25:05 -0500

    style updates

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
         "userId": {
           "email": {},
           "petList":{}
-            
+
         }
     }
 
-    
+
 /* global firebase moment */
 // Steps to complete:
 
@@ -70,13 +70,13 @@ $("#sign-in-btn").on("click", function (event) {
                 newUser.petList = "this is a pet";
 
             // Save user data to the database
-            // debugger; 
+            // debugger;
             database.ref().push(newUser);
 
             // Logs everything to console
             //console.log(newUser.id);
             //console.log(newUser.email);
-            
+
             // Alert
             alert("User " + userId + " successfully added");
 
@@ -101,16 +101,16 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
     var email = childSnapshot.val().email;
 
 
-    
+
     $("#user-table > tbody").append("<tr><td>" + userId + "</td><td>" + email + "</td></tr>");
 });
 
-// bc   
+// bc
 
     $(".js-search").on("click", function() {
             //Prevent the default function of button
-            event.preventDefault(); 
-            
+            event.preventDefault();
+
             //Console log that the putton was pushed
             console.log("The button was pushed!");
 
@@ -119,13 +119,13 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
             //Create a variable called searchTerm to hold the value that the user entered into the search box
             var searchTerm = $('.js-search-term').val().trim();
-            
-            //Console log the searchTerm    
+
+            //Console log the searchTerm
             console.log(searchTerm);
-           
+
             // Construct a queryURL using the search term
                 var queryURL = "https://dailymed.nlm.nih.gov/dailymed/services/v2/spls.json?drug_name=" + searchTerm + "&pagesize=5&page=2" ;
-    
+
             // Performing an AJAX request with the queryURL
             $.ajax({
                 url: queryURL,
@@ -135,14 +135,14 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
                 .then(function(response) {
                 console.log(queryURL);
                 console.log(response);
-                
+
                 // storing the data from the AJAX request in the results variable
                 console.log(response.data.title);
                 var results = response.data;
-    
+
                 // Looping through each result item
                 for (var i = 0; i < results.length; i++) {
-    
+
                 // Creating and storing a div tag
                 var drugDiv = $("<div>");
                 drugDiv.attr("data-id", i);
@@ -164,89 +164,89 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
                 //Creating a pargraph tag with the setID
                 var titleID  = $("<p>").text(setID).attr("id",i).hide();
-                
-      
+
+
                // Appending the paragraph to the drugDiv
-               
+
                 drugDiv.empty().append(addButton, title, titleID);
                 // drugDiv.prepend(b);
-                
+
               // Prepending the animalDiv to the HTML page in the "#gifs-appear-here" div
               $(".drugs-appear-here").prepend(drugDiv);
-                    
+
             };
-        });  
-    
+        });
+
         $(document).on('click', '.add', function() {
-    
+
              //Prevent the default function of button
-             event.preventDefault(); 
-            
+             event.preventDefault();
+
              //Console log that the add button was pushed
              console.log("The add button was pushed!");
-             
-            
+
+
              //Create a variable called searchTerm to hold the value that the user entered into the search box
 
             //  var addedDrugTerm = $(this).parent().get(0).innerText.slice(1);
               var clickedID = $(this).attr('id');
               var addedDrugTerm = $("div").find('[data-id='+ clickedID + ']');
-              
-             //Console log the addedDrugTerm  
+
+             //Console log the addedDrugTerm
              console.log("This is the added drug term: " + addedDrugTerm);
-    
+
             //  $("<button>").addClass("delete");
             //  console.log($("<button>"));
 
              // Creating and storing a div tag
              var usersDrugDiv = $("<div>").addClass("usersDrugDiv");
-      
+
             //  Creating a paragraph tag with the item the user wants to add to their list
             //  var p = $("<p>").text(addedDrugTerm);
             //  console.log(p);
-             
+
             //  Creating a delete button next to each drug name
             //  var bb = $("<button class='delete btn'>").text("-");
-             
+
             //  p.prepend(bb);
-    
-    
+
+
             // Appending the paragraph to the drugDiv
             // usersDrugDiv.prepend(bb);
             // usersDrugDiv.append(p);
             // usersDrugDiv.append(bb, addedDrugTerm)
             usersDrugDiv.append(addedDrugTerm)
-            
-             
+
+
            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
            $(".user-drugs-appear-here").prepend(usersDrugDiv);
-            
-          
+
+
         });
-        
-    
-    
+
+
+
         // function emptyDrugDiv() {
         //     $(".drugs-appear-here").empty();
         // }
-    
-    
-    }); 
-    
-    $('document').on('click','.delete', function(event) {
-   
-        //Prevent the default function of button
-        event.preventDefault(); 
 
-       
+
+    });
+
+    $('document').on('click','.delete', function(event) {
+
+        //Prevent the default function of button
+        event.preventDefault();
+
+
         //Console log that the button was pushed
         console.log("The delete button was pushed!");
 
         // console.log(this);
         // var removeDrugTerm = $(this).parent().get(0).html();
-        
+
         });
-    
+
 
         // Initialize Firebase
         // var config = {
@@ -259,12 +259,12 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
         // };
         // firebase.initializeApp(config);
 
-       
+
 
 
     $(".js-savelist").on("click", function() {
         //Prevent the default function of button
-        event.preventDefault(); 
+        event.preventDefault();
 
          database.ref().push(newUser);
         // database.ref().set({
@@ -279,20 +279,20 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
         //     "pageId": "23",
         //     "storeURL": "/app/cars/gallery"
         // });
-        
+
         //Console log that the putton was pushed
         console.log("The save button was pushed!");
-        
+
         // Create a variable to reference the database.
         // var database = firebase.database();
-       
+
          //Clear out firebase
         // database.ref().remove();
-        
+
 
         //save everything into variables for firebase
         // $(".usersDrugDiv").each(function(){
-        
+
         // var drugTitle = $(this).find('p:first').text();
         // console.log(drugTitle);
         // var setID = $(this).find("p:nth-of-type(2)").text();
@@ -303,14 +303,30 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
         //  } ;
         //  console.log (drugInfo);
         // return drugInfo;
-        
-        
-        
+
+
+    $(".js-email").on("click", function() {
+        //Prevent the default function of button
+        event.preventDefault();
+
+        //Console log that the putton was pushed
+        console.log("The email button was pushed!");
+        //modal trigger
+
+        });
+
+        $(document).on('click', '.delete', function() {
+        event.preventDefault();
+        console.log("the delete button was pushed!");
+        $(this).parent().empty();
+
+    });
+
         // });
         var userList=[];
 
         $(".usersDrugDiv").each(function(){
-        
+
             var drugTitle = $(this).find('p:first').text();
             console.log(drugTitle);
             var setID = $(this).find("p:nth-of-type(2)").text();
@@ -323,10 +339,10 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
             // return drugInfo;
             userList.push(drugInfo);
             console.log("This is my userList" + userList);
-            
-            
+
+
             });
-    
+
             // database.ref().push({ userList
 
             // });
@@ -334,7 +350,7 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
             $(".user-drugs-appear-here").empty();
             // database.ref().child("new").push(userList);
 
-            
+
             // .setValue(true)
         // function createArray(drugInfo) {
         //     var userList=[];
@@ -342,7 +358,7 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
         //     console.log("This is the user list " + userList);
         // };
 
-        // createArray(); 
+        // createArray();
 
         // $(".users-drugs-appear-here").each(function(drugInfo){
         //     var userList=[];
@@ -350,17 +366,17 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
         //     console.log("This is the user list " + userList);
 
         // });
-        
+
         // $(".users-drugs-appear-here").each(function(drugInfo){
-            
+
         //     var userList=[];
         //     userList.push(drugInfo);
         //     ref.child("currentUser").push(userList);
-        // }); 
+        // });
 
-    });   
-});    
-        
+    });
+});
+
     // var dataArray = new Array();
     // $('.popupDiv').each(function(){
     //   var dataLayer = $(this).data('layer');
@@ -373,6 +389,6 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
     //         //do your stuff here
     //      });
     //   }
-    // });  
+    // });
         //https://stackoverflow.com/questions/26271933/jquery-how-to-loop-through-elements-with-data-attribute
-        
+
